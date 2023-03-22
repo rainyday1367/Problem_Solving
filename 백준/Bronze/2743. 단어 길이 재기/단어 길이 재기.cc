2ec04f964d697fee31515ac0d2384e991c967304
DH <98398484+itjustrainyday@users.noreply.github.com>
2023-03-22 20:40:17 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0) -> sync_with_stdio(0)
+using namespace std;
+
+int main() {
+	fastio;
+	string s;
+	cin >> s;
+	int n = s.length();
+	cout << n << '\n';
+	return 0;
+}
