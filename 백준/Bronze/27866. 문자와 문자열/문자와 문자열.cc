75be8145d494c1b52d57e2a11c9f9764f7cf514f
DH <98398484+itjustrainyday@users.noreply.github.com>
2023-03-22 20:44:57 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0) -> sync_with_stdio(0)
+using namespace std;
+
+int main() {
+	fastio;
+	string s;
+	int n;
+	cin >> s >> n;
+	cout << s[n-1] << '\n';
+	return 0;
+}
