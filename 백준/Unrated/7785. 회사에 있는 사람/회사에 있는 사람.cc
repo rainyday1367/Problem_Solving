d31b6f39bb595738b0075a5c3bb7ff594a1e17da
DH <98398484+itjustrainyday@users.noreply.github.com>
2023-03-22 20:33:26 +0900

    Update 회사에 있는 사람.cc

--- 백준/Unrated/7785. 회사에 있는 사람/회사에 있는 사람.cc
+++ 백준/Unrated/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 	fastio;
-	set<string> s;
+	set<string> s;//연관 컨테이너, 균형 이진트리 노드 기반, key라 불리는 원소의 집함 중복 x
 	int n;
 	cin >> n;
 	while (n--) {
@@ -16,6 +16,6 @@ int main() {
 			s.erase(name);
 	}
 	for (auto i = s.rbegin(); i != s.rend(); ++i)
-		cout << *i << '\n';
+		cout << *i << '\n';//반대로 출력하기 위해 사용하는 rbegin과 rend 세트처럼
 	return 0;
 }
