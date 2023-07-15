02ce621d53e93f5a1a1fc675996e5a23d8bf7218
DH <98398484+itjustrainyday@users.noreply.github.com>
2023-07-15 23:09:33 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+	if (s == "SONGDO") cout << "HIGHSCHOOL";
+	else if (s == "CODE") cout << "MASTER";
+	else if (s == "2023") cout << "0611";
+	else if (s == "ALGORITHM") cout << "CONTEST";
+
+}
