77099c6d32723982429c0db78d5a4561f930beab
DH <98398484+itjustrainyday@users.noreply.github.com>
2023-07-26 23:36:00 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+int main() {
+	fastio;
+	int n;
+	cin >> n;
+	n = n - 1946;
+	cout << n;
+	return 0;
+}
