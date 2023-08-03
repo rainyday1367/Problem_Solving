3a9a2d5f9b4dfd551d2d90c3bb9192e232c180ee
DH <98398484+itjustrainyday@users.noreply.github.com>
2023-08-03 14:24:04 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
@@ -6,16 +6,15 @@ int main(void) {
     cin.tie(0);
     int n;
     cin >> n;
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= i; ++j) cout << "*";
-        for (int j = 1; j <= 2 * n - 2 * i; j++) cout << " ";
-        for (int j = 1; j <= i; ++j) cout << "*";
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < i+1; ++j) cout << "*";
+        for (int j = 0; j < 2*(n - i - 1); j++) cout << " ";
+        for (int j = 0; j < i+1; ++j) cout << "*";
         cout << '\n';
     }
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < (2 * n - 2 * i - 2)/2; ++j) cout << "*";
-        for (int j = 0; j <= i; ++j) cout << " ";
-        for (int j = 0; j <= i; ++j) cout << " ";
+    for (int i = 0; i < n-1 ; ++i) {
+        for (int j = 0; j < (n - i - 1); ++j) cout << "*";
+        for (int j = 0; j < 2*(i + 1); ++j) cout << " ";
         for (int j = 0; j < (2 * n - 2 * i - 2)/2; ++j) cout << "*";
         cout << '\n';
     }
