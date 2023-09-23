efd3843cb5e631e7937eb5e011a5c1d0ff45c88a
DH <98398484+itjustrainyday@users.noreply.github.com>
2023-09-23 16:22:15 +0900

    [Silver V] Title: 생일, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/5635. 생일/생일.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	int n;
+	cin >> n;
+	vector<pair<pair<int, int>, pair<int, string>>> v(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> v[i].second.second >> v[i].second.first >> v[i].first.second >> v[i].first.first;
+	}
+	sort(v.begin(), v.end());
+	cout << v[n-1].second.second << '\n' << v[0].second.second;
+}
