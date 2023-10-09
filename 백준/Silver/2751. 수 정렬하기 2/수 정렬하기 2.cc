f8253e84d8615e9b1f7c9ce279dcdd3fe38ad2a9
DH <98398484+itjustrainyday@users.noreply.github.com>
2023-10-09 16:47:49 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 280 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < n; i++) cout << arr[i] << '\n';
+}
