9fc5cad47f25640a77e514246af8e58da36a151e
DH <98398484+itjustrainyday@users.noreply.github.com>
2023-10-10 21:55:00 +0900

    [Silver V] Title: 단어 정렬, Time: 20 ms, Memory: 3976 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,23 +1,24 @@
-// Authored by : tongnamuu
-// Co-authored by : -
-// http://boj.kr/e365634189b1497a95abaf856df7758f
-#include <bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
-
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  int n;
-  cin>>n;
-  vector<string>a(n);
-  for(int i=0;i<n;++i) cin>>a[i];
-  sort(a.begin(), a.end(), [](const string& u, const string& v){
-    int ul = u.length();
-    int vl = v.length();
-    if(ul!=vl) return ul < vl;
-    return u < v;
-  });
-  a.erase(unique(a.begin(), a.end()), a.end()); // a에서 중복된 원소를 제거하는 명령
-  for(string& s : a) cout<<s<<'\n';
-  return 0;
+bool cmp(const string& a, const string& b) {
+	int lena = a.size(); int lenb = b.size();
+	if (lena != lenb) return lena < lenb;
+	else {
+		return a < b;
+	}
+}
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	vector<string> v(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> v[i];
+	}
+	sort(v.begin(), v.end(), cmp);
+	v.erase(unique(v.begin(), v.end()), v.end());
+	for (auto& i : v) cout << i << '\n';
 }
