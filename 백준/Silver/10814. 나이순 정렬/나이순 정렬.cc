d15f3155971dfdc517f3dfad3ce1acac3211296b
DH <98398484+itjustrainyday@users.noreply.github.com>
2023-10-11 01:47:20 +0900

    [Silver V] Title: 나이순 정렬, Time: 44 ms, Memory: 9840 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+    vector<tuple<int, int, string>> members;
+    for (int i = 0; i < N; i++) {
+        int age;
+        string name;
+        cin >> age >> name;
+        members.push_back({ age, i, name });
+    }
+    sort(members.begin(), members.end());
+    for (auto [age, _, name] : members)
+        cout << age << " " << name << '\n';
+}
