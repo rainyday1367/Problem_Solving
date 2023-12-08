0e4c72d28cf771afa7be0369b8310ff4a48332ee
DH <98398484+itjustrainyday@users.noreply.github.com>
2023-12-09 01:43:14 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	int r, s;
+	cin >> r >> s;
+	cout << 2 * s - r << '\n';
+	return 0;
+}
