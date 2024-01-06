0d475d8e4044a5c6b6d72f1c6c0667ae817c2fa4
DH <98398484+itjustrainyday@users.noreply.github.com>
2024-01-06 22:37:24 +0900

    [Bronze V] Title: 연세대학교, Time: 204 ms, Memory: 17548 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if(n == 0) {
+			System.out.println("YONSEI");
+		}
+		else if (n == 1) {
+			System.out.println("Leading the Way to the Future");
+		}
+		return;
+	}
+}
