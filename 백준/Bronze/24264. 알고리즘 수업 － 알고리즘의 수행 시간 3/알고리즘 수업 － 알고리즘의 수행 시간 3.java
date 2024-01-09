b1c7e75e01b9779e87af16e410cf464a11583569
DH <98398484+itjustrainyday@users.noreply.github.com>
2024-01-09 22:34:05 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 212 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		System.out.println(n*n);
+		System.out.print(2);
+		sc.close();
+	}
+}
