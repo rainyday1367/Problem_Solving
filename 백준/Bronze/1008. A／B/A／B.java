8ca1eafbb0d7a37c472e35bafa21661c44b2cb3d
DH <98398484+itjustrainyday@users.noreply.github.com>
2024-01-22 21:08:00 +0900

    [Bronze V] Title: A/B, Time: 228 ms, Memory: 17944 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		double n = sc.nextDouble();
+		double m = sc.nextDouble();
+		System.out.println(n/m);
+	}
+}
