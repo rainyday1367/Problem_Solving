b534cd86ce8c36f1ae2e4e9bc8eac544c8c5fc28
DH <98398484+itjustrainyday@users.noreply.github.com>
2024-01-24 20:19:17 +0900

    [Bronze V] Title: ??!, Time: 216 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String ns = sc.nextLine();
+		System.out.println(ns +"??!");
+		
+	}
+
+}
