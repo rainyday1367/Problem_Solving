8aadfb26fec2d03cd39d3dba1a21ad53f962bcde
DH <98398484+itjustrainyday@users.noreply.github.com>
2024-03-20 20:27:56 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 3.71 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "";
+    if(num % 2 == 0){
+        answer = "Even";
+    }
+    else{
+        answer = "Odd";
+    }
+    return answer;
+}
+int main(){
+    int num = 0;
+    cin >> num;
+    string ans = solution(num);
+    cout << num << '\n';
+    return 0;
+}
