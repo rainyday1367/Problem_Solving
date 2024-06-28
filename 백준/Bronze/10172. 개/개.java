ecc64e1c7a5c6581753af0b397ab424af97d3150
DH <98398484+itjustrainyday@users.noreply.github.com>
2024-06-28 15:07:16 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\"); //큰따음표, 백슬래쉬는 백슬래쉬를 앞에 넣어야한다.
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
