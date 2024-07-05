10c0dfded027001bc08d8bdd205dd22a8285b0df
DH <98398484+itjustrainyday@users.noreply.github.com>
2024-07-05 23:45:24 +0900

    [Bronze I] Title: 평균, Time: 300 ms, Memory: 23384 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		double[] arr = new double[sc.nextInt()];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextDouble();
+		}
+		
+		sc.close();
+		
+		double sum = 0;
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < arr.length; i++) {
+			sum += ((arr[i] / arr[arr.length - 1]) * 100);
+		}
+		System.out.println(sum / arr.length);
+	}
+
+}
